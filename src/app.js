5d5dbd1bb0738f698e391019a1bd8cd0038cd59e
Jacksson Sánchez <info@creativoz.es>
2025-03-16 23:12:59 +0000

    GeneradorExcusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = () => generateExcuse();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+        const generateExcuse = () => {
+            let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+            let action = ['ate', 'peed on', 'crushed', 'broke'];
+            let what = ['my homework', 'my phone', 'the car'];
+            let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+            const randomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+            let excuse = `${randomElement(who)} ${randomElement(action)} ${randomElement(what)} ${randomElement(when)}.`;
+            
+            document.getElementById('excuse').innerHTML = excuse;
+        };
